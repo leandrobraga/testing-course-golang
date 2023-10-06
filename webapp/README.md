6a2a88b9cb5f86b898ae0c8aa94d97b3033b3d4b
Leandro Braga <leandro.quadros@gmail.com>
2023-10-06 19:07:46 +0100

    Start very basic web server with chi mux

---
+++ webapp/README.md
@@ -0,0 +1,8 @@
+# Web Application
+
+Learn how to write effective unit and integration tests in Go, for web applications and REST APIs.
+https://www.udemy.com/course/introduction-to-testing-in-go-golang/
+
+## Description
+
+A simple web app with route, handler and midleware for trainning test.
---
+++ webapp/cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type application struct{}
+
+func main() {
+	app := application{}
+
+	mux := app.routes()
+
+	log.Println("Starting server on port 8080...")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
