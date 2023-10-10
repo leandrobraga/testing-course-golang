31e2fe07a68a4969e4d5d0ef0939beebfd7d2a59
Leandro Braga <leandro.quadros@gmail.com>
2023-10-10 21:44:45 +0100

    Setting up database connection

---
+++ webapp/cmd/web/db.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/pgconn"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func OpenDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func (app *application) connectToDB() (*sql.DB, error) {
+	connection, err := OpenDB(app.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to Postgres!")
+
+	return connection, nil
+}
--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,18 +11,30 @@ import (
 
 type application struct {
 	Session *scs.SessionManager
+	DSN     string
+	DB      *sql.DB
 }
 
 func main() {
-	app := application{
-		Session: getSession(),
+
+	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	app.Session = getSession()
+	app.DB = conn
 
 	mux := app.routes()
 
 	log.Println("Starting server on port 8080...")
 
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
