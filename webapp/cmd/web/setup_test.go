dcff5520e792902bd633be8800da9591159ec5f8
Leandro Braga <leandro.quadros@gmail.com>
2023-10-11 21:47:14 +0100

    Add test login

--- webapp/cmd/web/setup_test.go
+++ webapp/cmd/web/setup_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	"github.com/leandrobraga/testing-course-golang/webapp/pkg/db"
 )
 
 var app application
@@ -11,6 +14,16 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
+	app.DSN = "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.Session = getSession()
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
